c49e89dd8264bfc9b4bd053ea4b7327159ce0640
bryan.pilco <bpilco@cobiscorp.com>
2022-01-06 23:42:08 -0500

    primer commit

---
+++ src/app/Heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+
+  heroes: string[]= ['Spiderman', 'Hulk', 'Thor'];
+
+  heroeBorrado: string = '';
+
+  borrarHeroe(): string{
+    this.heroeBorrado = this.heroes.shift() || '';
+    return this.heroeBorrado;
+  }
+}
+
+
